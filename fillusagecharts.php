bc83bf5e455d1b792a42bcb825361f9f22cb20a5
arnovr <arnovanrossum1@gmail.com>
2014-09-21 21:38:50 +0200

    Updated tool to add alot more values

--- fillusagecharts.php
+++ fillusagecharts.php
@@ -8,18 +8,26 @@ $connect = mysqli_connect('localhost', $CONFIG['dbuser'], $CONFIG['dbpassword'],
 
 $usernames = array('admin', 'test1', 'test2', 'test3');
 
-for($i = 1; $i < 13; $i++)
+$now = new \DateTime();;
+foreach(array(2012,2013,2014) as $year)
 {
-    for($j = 1; $j < 31; $j++)
-    {
-        $created = new \DateTime();
-        $created->setDate(2014, $i, $j);
-        foreach($usernames as $username)
-        {
-            $usage = rand(10000, 2000000000);
-            $sql = 'INSERT INTO oc_uc_storageusage (created, username, `usage`) VALUES ("'. $created->format('Y-m-d 12:00:00') . '", "' . $username . '", ' . $usage . ');';
-            $result = mysqli_query($connect, $sql);
-        }
-    }
+	for($i = 1; $i < 13; $i++)
+	{
+	    for($j = 1; $j < 31; $j++)
+	    {
+	        $created = new \DateTime();
+	        $created->setDate($year, $i, $j);
+		if ( $now < $created)
+		{
+			exit;
+		}
+	        foreach($usernames as $username)
+	        {
+	            $usage = rand(10000, 2000000000);
+	            $sql = 'INSERT INTO oc_uc_storageusage (created, username, `usage`) VALUES ("'. $created->format('Y-m-d 12:00:00') . '", "' . $username . '", ' . $usage . ');';
+	            $result = mysqli_query($connect, $sql);
+	        }
+	    }
+	}
 }
-mysqli_query($connect, $sql);
+
